720105db635ccf34802d6bf3c5750a9c8bbe197c
MehmetBulutt <mehmetbulut7176@gmail.com>
2025-05-15 19:49:35 +0300

    React ile arayüzleri başlandı

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
   function App() {
   return (
     <>
@@ -7,24 +9,51 @@
   )
 }
 
-function Header (){
-  return(
+function Header() {
+  return (
+   <h1>Header</h1>  
+  );
+}
+
+function ProductList() {
+
+   const [products, setProducts] = useState([
+    { id:1 ,name: "product 1", price: 1000, is_active: true },
+    { id:2 ,name: "product 2", price: 2000, is_active: false },
+    { id:3 ,name: "product 3", price: 3000, is_active: true },
+ ]);
+  
+  function addProduct() {
+    setProducts([...products,  { id:Date.now() ,name: "product 4", price: 4000, is_active: true }])
+  }
+
+  console.log("render...")
+
+  return (
     <div>
-   <h1>Header</h1>
-   <Product />
-   </div>
+      <h2>ProductList</h2>
+
+      { products.map(p => (
+         <Product key={p.id} product={p}/> 
+      )) }
+
+      <button onClick={ addProduct }>Add Product</button>
+  
+    </div>
   );
 }
 
-function ProductList (){
-  return(
-    <h2>ProductList</h2>
-  );
-}
-
-function Product (){
-  return(
-    <h3>Product</h3>
+function Product(props : any) {
+  return (
+    <>
+      { props.product.is_active ? (
+        <div>
+          <h3>{ props.product.name }</h3>
+          <p>{ props.product.price }</p>
+        </div> 
+      ) : <p>ürün satışta değil</p>
+      }
+    </>
   );
 }
 
